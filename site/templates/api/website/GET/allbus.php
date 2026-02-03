a060e23c21fdbd87db81bb28ecbf9a6c39f57f28
Udnlx <Udnlx@yandex.ru>
2026-02-03 10:49:30 +0300

    переделана апи под формирование рейсов из таблицы цен

--- site/templates/api/website/GET/allbus.php
+++ site/templates/api/website/GET/allbus.php
@@ -5,10 +5,34 @@ namespace ProcessWire;
 $bus_pages = $pages->find("template=buses_item, sort=sort");
 $all_bus = [];
 foreach ($bus_pages as $bus) {
+	$table_bus = $bus->table_price;
+	foreach ($table_bus as $item) {
+	$departure = $item->name_station;
+	$departure_point = mb_substr($departure, 0, mb_strlen($departure, 'UTF-8') - 5, 'UTF-8'); 
+	$departure_time = mb_substr($departure, -5, null, 'UTF-8');
+	$departure_station = $item->departure_station;
+
+	$destination = $item->name_station_finish;
+	$destination_point = mb_substr($destination, 0, mb_strlen($destination, 'UTF-8') - 5, 'UTF-8');
+	$destination_time = mb_substr($destination, -5, null, 'UTF-8');
+	$destination_station = $item->destination_station;
+
 	$all_bus[] = [
-		"id" => $bus->id,
-		"name" => $bus->title,
+	"id" => $bus->id,
+	"name" => $departure_point . ' - ' . $destination_point,
+	"departurePoint" => $departure_point,
+	"departureTime" => $departure_time,
+	"departureStation" => $departure_station,
+	"travelTime" => $item->travel_time,
+	"nextDay" => $item->next_day,
+	"destinationPoint" => $destination_point,
+	"destinationTime" => $destination_time,
+	"destinationStation" => $destination_station,
+	"price" => $item->price_ticket,
+	"carrier" => $bus->carrier,
 	];
+
+	}
 }
 
 $result["allBus"] = $all_bus;
--- site/templates/api/website/GET/allbusondate.php
+++ site/templates/api/website/GET/allbusondate.php
@@ -8,26 +8,51 @@ if ($input->get['departure']) {
 	$all_bus_ondate = [];
 
 	foreach ($bus_pages as $bus) {
-		$departure = $input->get['departure'];
-		$departure_exp = explode("-", $departure);
-		$month = $departure_exp[1];
-		$day = $departure_exp[2];
+		$departure_date = $input->get['departure'];
+		$departure_date_exp = explode("-", $departure_date);
+		$month = $departure_date_exp[1];
+		$day = $departure_date_exp[2];
 
 		foreach($bus->bus_calendar as $bc_item) {
 			if ($bc_item->month == $month) {
 				$arr_bus_calendar_days = explode(',', $bc_item->days);
 				foreach ($arr_bus_calendar_days as $bc_day) {
-					$bus_tickets_ondate = $pages->find("template=purchased_tickets, id_bus=" . $bus . ", date_depart=" . $departure . "");
-					$count_tickets = count($bus_tickets_ondate);
-					$max_seat = 53;
-					$free_seat = $max_seat - $count_tickets;
 					if ($bc_day == $day) {
-						$all_bus_ondate[] = [
+						$bus_tickets_ondate = $pages->find("template=purchased_tickets, id_bus=" . $bus->id . ", date_depart=" . $departure_date . "");
+						$count_tickets = count($bus_tickets_ondate);
+						$max_seat = 53;
+						$free_seat = $max_seat - $count_tickets;
+
+						$table_bus = $bus->table_price;
+						foreach ($table_bus as $item) {
+							$departure = $item->name_station;
+							$departure_point = mb_substr($departure, 0, mb_strlen($departure, 'UTF-8') - 5, 'UTF-8'); 
+							$departure_time = mb_substr($departure, -5, null, 'UTF-8');
+							$departure_station = $item->departure_station;
+
+							$destination = $item->name_station_finish;
+							$destination_point = mb_substr($destination, 0, mb_strlen($destination, 'UTF-8') - 5, 'UTF-8');
+							$destination_time = mb_substr($destination, -5, null, 'UTF-8');
+							$destination_station = $item->destination_station;
+
+							$all_bus_ondate[] = [
 							"id" => $bus->id,
-							"name" => $bus->title,
+							"name" => $departure_point . ' - ' . $destination_point,
+							"departurePoint" => $departure_point,
+							"departureTime" => $departure_time,
+							"departureStation" => $departure_station,
+							"travelTime" => $item->travel_time,
+							"nextDay" => $item->next_day,
+							"destinationPoint" => $destination_point,
+							"destinationTime" => $destination_time,
+							"destinationStation" => $destination_station,
+							"price" => $item->price_ticket,
+							"carrier" => $bus->carrier,
 							"freeSeats" => $free_seat,
-							"departure" => $departure,
-						];
+							"departure" => $departure_date,
+							];
+
+						}
 					}
 				}
 			}
